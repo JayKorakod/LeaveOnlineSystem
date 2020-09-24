c65ad9633850066016a6f7ad7dfea80b25998cab
sarawut jumnongnok <sarawutju@utacgroup.com>
2020-09-24 16:20:59 +0700

    Leave Data Repo

---
+++ LeaveSystemOnline/Repositories/LeaveDataRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeaveSystemOnline.Repositories
+{
+    public class LeaveDataRepo
+    {
+        LEAVE_STSTEM_ONLINEEntities context = new LEAVE_STSTEM_ONLINEEntities();
+
+        public void Create(LEAVEDATA model)
+        {
+            context.LEAVEDATA.Add(model);
+        }
+    }
+}
