476dac69ddb94cf4a37ed3d293215bc5100a83d8
JayKorakod <korakod.kaisri.gmail.com>
2020-10-09 15:45:43 +0700

    commit

--- LeaveSystemOnline/LeaveSystemOnlineModel.Designer.cs
+++ LeaveSystemOnline/LeaveSystemOnlineModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\SE\Leave System Online\LeaveSystemOnline\LeaveSystemOnlineModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\source\repos\LeaveSystemOnline\LeaveSystemOnline\LeaveSystemOnlineModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
