3d75d6fb5dde0311c3ecaba8d94d37fb5b0df217
JayKorakod <korakod.kaisri.gmail.com>
2020-11-06 17:31:52 +0700

    เพิ่มกราฟวงกลมเข้ามา

--- LeaveSystemOnline/Controllers/ReportLeaveDataController.cs
+++ LeaveSystemOnline/Controllers/ReportLeaveDataController.cs
@@ -14,7 +14,7 @@ namespace LeaveSystemOnline.Controllers
         LeaveDataService services = new LeaveDataService();
         LeaveDataRepo repo = new LeaveDataRepo();
         // GET: ReprotLeaveData
-        public ActionResult Index()
+        public ActionResult ShowPieChart()
         {
             return View();
         }
@@ -24,5 +24,42 @@ namespace LeaveSystemOnline.Controllers
             list = context.LEAVEDATA.Where(x => x.EMPLOYEE.department.StartsWith(search) || search == null).ToList();
             return View(list);
         }
+
+        public ActionResult PieChart()
+        {
+            int engineer = context.LEAVEDATA.Where(x => x.EMPLOYEE.department == "วิศวกรรม").Count();
+            int it = context.LEAVEDATA.Where(x => x.EMPLOYEE.department == "เทคโนโลยีสารสนเทศ").Count();
+            int purchase = context.LEAVEDATA.Where(x => x.EMPLOYEE.department == "จัดซื้อ").Count();
+            int warehouse = context.LEAVEDATA.Where(x => x.EMPLOYEE.department == "คลังสินค้า").Count();
+            int hr = context.LEAVEDATA.Where(x => x.EMPLOYEE.department == "บุคคล").Count();
+            int marketing = context.LEAVEDATA.Where(x => x.EMPLOYEE.department == "การตลาด").Count();
+            int security = context.LEAVEDATA.Where(x => x.EMPLOYEE.department == "รักษาความปลอดภัย").Count();
+            int production = context.LEAVEDATA.Where(x => x.EMPLOYEE.department == "ฝ่ายผลิต").Count();
+
+            Ratio obj = new Ratio();
+            obj.Engineer = engineer;
+            obj.IT = it;
+            obj.Purchase = purchase;
+            obj.Warehouse = warehouse;
+            obj.HR = hr;
+            obj.Marketing = marketing;
+            obj.Security = security;
+            obj.Production = production;
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public class Ratio
+        {
+            public int Engineer { get; set; }
+            public int IT { get; set; }
+            public int Purchase { get; set; }
+            public int Warehouse { get; set; }
+            public int HR { get; set; }
+            public int Marketing { get; set; }
+            public int Security { get; set; }
+            public int Production { get; set; }
+        }
+
     }
 }
